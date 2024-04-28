28c5b5841119a64781f04e3c4e77f6a8dddf7dda
darshit <sojitradarshitpiyushbhai@gmail.com>
2024-04-28 10:47:48 +0530

    experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -25,17 +25,17 @@ const Experience = () => {
             </article>
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
-              <h4>Bootstrap, Tailwind</h4>
+              <h4>Tailwind</h4>
             </article>
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
-              <h4>React , Vite JS, Next Js</h4>
+              <h4>React , Vite JS</h4>
             </article>
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Typescript</h4>
             </article>
-            <article className="experience__details">
+            {/* <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Firebase</h4>
             </article>
@@ -44,13 +44,12 @@ const Experience = () => {
               <h4>Node.js, Express.js</h4>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill className="experience__details-icon" />
-              <h4>Responsive Design, BEM Methodology</h4>
+<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="experience__details-icon" height="18" width="18" xmlns="http://www.w3.org/2000/svg"><path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"></path></svg>              <h4>Responsive Design, BEM Methodology</h4>
             </article>
             <article className="experience__details">
               <BsFillPatchCheckFill className="experience__details-icon" />
               <h4>Redux, Redux-toolkit, Context API</h4>
-            </article>
+            </article> */}
           </div>
         </div>
       </div>
